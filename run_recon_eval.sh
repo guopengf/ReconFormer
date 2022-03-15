65e78f3bfc0995254b21f8d01c5991ef110f76da
pengfei <pguo4@jhu.edu>
2022-03-15 15:01:38 -0400

    update

--- run_recon_eval.sh
+++ run_recon_eval.sh
@@ -1,3 +1,3 @@
 #!/bin/env bash
 # ReconFormer Evaluation
-python main_recon_test.py --phase test --model ReconFormer --challenge singlecoil --F_path /mnt/2t/MR_data/fastMRI_preprocessed --test_dataset F --sequence PD --accelerations 8 --center-fractions 0.04 --checkpoint /home/pengfei/code/exp_recon_seg/media_release/F_RTransv2_1206_ms_recatt_8/checkpoint.pth --verbose
+python main_recon_test.py --phase test --model ReconFormer --challenge singlecoil --F_path 'path to fastMRI dataset' --test_dataset F --sequence PD --accelerations 8 --center-fractions 0.04 --checkpoint 'path to checkpoint'--verbose
--- run_recon_exp.sh
+++ run_recon_exp.sh
@@ -1,5 +1,5 @@
 #!/bin/env bash
 # ReconFormer x4
-python main_recon.py --phase train --model ReconFormer --epochs 50 --challenge singlecoil --bs 4 --F_path /mnt/2t/MR_data/fastMRI_preprocessed --train_dataset F --test_dataset F --sequence PD --accelerations 4 --center-fractions 0.08 --lr 0.0002 --lr-step-size 5 --lr-gamma 0.9 --save_dir /home/F_ReconRelease --verbose
+python main_recon.py --phase train --model ReconFormer --epochs 50 --challenge singlecoil --bs 4 --F_path 'path to fastMRI dataset' --train_dataset F --test_dataset F --sequence PD --accelerations 4 --center-fractions 0.08 --lr 0.0002 --lr-step-size 5 --lr-gamma 0.9 --save_dir /home/pengfei/F_ReconRelease --verbose
 # ReconFormer x8
-#python main_recon.py --phase train --model ReconFormer --epochs 50 --challenge singlecoil --bs 4 --F_path /mnt/2t/MR_data/fastMRI_preprocessed --train_dataset F --test_dataset F --sequence PD --accelerations 8 --center-fractions 0.04 --lr 0.0002 --lr-step-size 5 --lr-gamma 0.9 --save_dir /home/F_ReconRelease --verbose
+#python main_recon.py --phase train --model ReconFormer --epochs 50 --challenge singlecoil --bs 4 --F_path 'path to fastMRI dataset' --train_dataset F --test_dataset F --sequence PD --accelerations 8 --center-fractions 0.04 --lr 0.0002 --lr-step-size 5 --lr-gamma 0.9 --save_dir /home/pengfei/F_ReconRelease --verbose
